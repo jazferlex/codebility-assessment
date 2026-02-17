f13643ed3e586180ef2858d98169934e652f3b49
Jazfer Licaros <jazferlex@gmail.com>
2026-02-17 22:45:50 +0800

    Implement simple blog with Next.js

--- fullstack/app/layout.tsx
+++ fullstack/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,10 +34,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
+    <html lang="en" >
+      <Head>
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
-      </head>
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
